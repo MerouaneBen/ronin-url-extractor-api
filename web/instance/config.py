6add206620ec45b20dbc365c7f9f97bfd33019fd
Merouane Benthameur <merouane.benthameur@exfo.com>
2021-08-29 19:44:49 -0400

    fix syntax

--- web/instance/config.py
+++ web/instance/config.py
@@ -5,5 +5,4 @@ import os
 class Config(object):
     SECRET_KEY = os.getenv('SECRET')
     API_PORT = os.getenv('API_PORT')
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + os.getenv('MYSQL_USER')+ ":" + os.getenv('MYSQL_PASSWORD') + "@" +
-                       os.getenv('MYSQL_HOST') + ":" + os.getenv('MYSQL_PORT') + "/" + Config.MYSQL_DATABASE + "?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + os.getenv('MYSQL_USER')+ ":" + os.getenv('MYSQL_PASSWORD') + "@" + os.getenv('MYSQL_HOST') + ":" + os.getenv('MYSQL_PORT') + "/" + Config.MYSQL_DATABASE + "?charset=utf8mb4"
